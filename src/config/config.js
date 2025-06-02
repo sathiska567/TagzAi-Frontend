225390d72a425f72edc938559b83aac3750292f0
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2025-06-03 00:27:00 +0530

    ip updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL_Auth = "http://54.165.182.229:5001/auth";
-export const API_BASE_URL_User = "http://54.165.182.229:5002/user";
-export const API_BASE_URL_Image = "http://54.165.182.229:5003/image";
-export const API_BASE_URL_Payment = "http://54.165.182.229:5004/payment";
+export const API_BASE_URL_Auth = "http://3.93.232.186:5001/auth";
+export const API_BASE_URL_User = "http://3.93.232.186:5002/user";
+export const API_BASE_URL_Image = "http://3.93.232.186:5003/image";
+export const API_BASE_URL_Payment = "http://3.93.232.186:5004/payment";
